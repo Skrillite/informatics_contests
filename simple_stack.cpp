406c7700a57f519c682501a2a4237bafc6317688
skrillite <nazarov.k.k@gmail.com>
2020-04-09 21:17:09 +0500

    ..

--- simple_stack.cpp
+++ simple_stack.cpp
@@ -4,30 +4,30 @@
 using namespace std;
 
 struct Node {
-	int value;
+	int data;
 	Node* prev_node;
 
-	Node(int value, Node* prev_node)
-		: value(value)
-		, prev_node(prev_node) {}
+	Node ( int& _data, Node* _prev_node )
+		: data      ( _data      )
+		, prev_node ( _prev_node ) {}
 };
 
 struct Stack {
-	void push(int n);
-	void pop  ();
-	void back ();
-	void size ();
-	void clear();
-	bool empty();
+	void push ( int n );
+	void pop   ();
+	void back  ();
+	void size  ();
+	void clear ();
+	bool empty ();
 
-	Stack()
-		: last(nullptr) {}
+	Stack ()
+		: last ( nullptr ) {}
 private:
 	Node* last;
 };
 
 
-int main() {
+int main () {
 	string command;
 	int arg;
 	Stack stack;
@@ -35,53 +35,50 @@ int main() {
 		cin >> command;
 		if (command == "push") {
 			cin >> arg;
-			stack.push(arg);
+			stack.push ( arg );
 		}
-		else if (command == "pop"  )	stack.pop  ();
-		else if (command == "back" )	stack.back ();
-		else if (command == "size" )	stack.size ();
-		else if (command == "clear")	stack.clear();
+		else if (command == "pop")	stack.pop   ();
+		else if (command == "back")	stack.back  ();
+		else if (command == "size")	stack.size  ();
+		else if (command == "clear")stack.clear ();
 	}
-	cout << "bye"; 
+	cout << "bye";
 }
 
-bool Stack::empty() {
+bool Stack::empty () {
 	if (last == nullptr) return true;
 	return false;
 }
 
-void Stack::push(int n) {
-	if (!empty()) {
-		last = new Node(n, last);
+void Stack::push ( int n ) {
+	if (!empty ()) {
+		last = new Node ( n, last );
 	} else {
-		last = new Node(n, nullptr);
+		last = new Node ( n, nullptr );
 	}
 	cout << "ok" << endl;
 }
 
-void Stack::pop() {
-	if (!empty()) {
-		cout << last->value << endl;
-		if (last->prev_node != nullptr) {
-			Node* tmp = last;
-			last = last->prev_node;
-			delete tmp;
-		}
-		else {
-			delete last;
-			last = nullptr;
-		}
-	} else cout << "error" << endl;
+void Stack::pop () {
+	if (!empty ()) {
+		cout << last->data << endl;
+
+		Node* tmp = last;
+		last = last->prev_node;
+		delete tmp;
+	} else
+        cout << "error" << endl;
 }
 
-void Stack::back() {
-	if (!empty()) {
-		cout << last->value << endl;
-	} else cout << "error" << endl;
+void Stack::back () {
+	if (!empty ()) {
+		cout << last->data << endl;
+	} else
+        cout << "error" << endl;
 }
 
-void Stack::size() {
-	int counter(0);
+void Stack::size () {
+	int counter (0);
 	Node* now = last;
 	while (now != nullptr) {
 		counter++;
@@ -90,15 +87,13 @@ void Stack::size() {
 	cout << counter << endl;
 }
 
-void Stack::clear() {
-	Node* now = last;
+void Stack::clear () {
 	Node* del;
-	while (now != nullptr) {
-		del = now;
-		now = now->prev_node;
+	while (last != nullptr) {
+		del = last;
+		last = last->prev_node;
 		delete del;
 	}
 
-	last = nullptr;
 	cout << "ok" << endl;
 }
